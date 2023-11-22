c890b808ea9845822c9eea65c09c4b9ac80441ba
mazouari <mohamed.ali.zouari@gmail.com>
2023-11-22 22:19:38 +0100

    add todo

--- src/main/java/com/todo/pages/TodoPage.java
+++ src/main/java/com/todo/pages/TodoPage.java
@@ -2,6 +2,7 @@ package com.todo.pages;
 
 import java.io.IOException;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -17,16 +18,40 @@ public class TodoPage extends BasePage{
 	
 	/*Locators*/
 	final static String INPUT_TEXT = "//input[@ng-model= 'newTodo']";
+	final static String TODO_1 = "/html/body/ng-view/section/section/ul/li/div/label";
+	final static String CHECKBOX = "//input[@type= 'checkbox']";
 	
 	
 	@FindBy(how = How.XPATH, using = INPUT_TEXT)
 	public static WebElement inputText;
 	
+	@FindBy(how = How.XPATH, using = CHECKBOX)
+	public static WebElement textTodo1;
+	
+	@FindBy(how = How.XPATH, using = TODO_1)
+	public static WebElement checkbox;
+	
 	
 	/*Methods*/
 	public void fillTodo(String todo) {
 		inputText.sendKeys(todo);
-		
+		inputText.sendKeys(Keys.ENTER);
 	}
+	
+	public Boolean isElementDisplayed(WebElement element) {
+		Boolean isElementDisplayed = element.isDisplayed();
+		return isElementDisplayed;
+	}
+	
+	public String checkElementContains(WebElement element) {
+		String elementTodo = element.getText();
+		return elementTodo;
+	}
+	
+	public Boolean isCheckBoxSelected(WebElement element) {
+		Boolean isCheckBoxSelected = element.isSelected();
+		return isCheckBoxSelected;
+	}
+	
 
 }
--- src/test/java/com/todo/tests/AddTodoTest.java
+++ src/test/java/com/todo/tests/AddTodoTest.java
@@ -2,6 +2,7 @@ package com.todo.tests;
 
 import java.io.IOException;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.todo.pages.TodoPage;
@@ -19,7 +20,13 @@ public class AddTodoTest extends Setup{
 	public void iCanAddTodo() throws IOException {
 		
 		todoPage = new TodoPage();
+		boolean inputField = todoPage.isElementDisplayed(TodoPage.inputText);
+		Assert.assertTrue(inputField);
 		todoPage.fillTodo(prop.getProperty("todo1"));
+		String elementTodo = todoPage.checkElementContains(TodoPage.textTodo1);
+		Assert.assertTrue(elementTodo.contains(prop.getProperty("todo1")));
+		boolean checkbox = todoPage.isCheckBoxSelected(TodoPage.checkbox);
+		Assert.assertTrue(checkbox);
 	}
 
 }
